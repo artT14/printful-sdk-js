5bf36596f657e49852f4a8c6e3b906d7549f52c1
artT14 <lamferme@gmail.com>
2023-07-12 12:46:34 -0700

    ShippingRateAPI

--- src/lib/shipping-rate.ts
+++ src/lib/shipping-rate.ts
@@ -1,6 +1,7 @@
 import GenericAPI from "./generic";
 import fetch from 'cross-fetch';
 import type { Headers } from "../types/headers";
+import type { ShippingInfo } from "../types/shipping";
 
 //------------------------------------------------------------------------------------------------------//
 // VII. SHIPPING RATE API
@@ -8,5 +9,23 @@ import type { Headers } from "../types/headers";
 export default class ShippingRateAPI extends GenericAPI{
     constructor(headers: Headers, origin: string){super(headers, origin)}
 
-    async calculateShipping(){}
+    /**
+     * Returns available shipping options and rates for the given list of products.
+     * 
+     * @param {ShippingInfo} shipping_info - Recipient location information
+     */
+    async calculateShipping(shipping_info: ShippingInfo){
+        const url = this.origin+"/shipping/rates";
+        const response = await fetch(url, {
+            method: "POST",
+            headers: this.headers,
+            body: JSON.stringify(shipping_info)
+        });
+        const data = await response.json();
+        const {code, result: shipping_options, error} = await data;
+        if (code >= 400){
+            return {shipping_options: [], error};
+        }
+        return {shipping_options, error: {}};
+    }
 }
---
+++ src/types/shipping.ts
@@ -0,0 +1,23 @@
+export type ShippingInfo = {
+    recipient: ShippingRecipient,
+    items: Array<ShippingItem>,
+    currency: string,
+    locale: string
+}
+
+export type ShippingRecipient = {
+    address1: string,
+    city: string,
+    country_code: string,
+    state_code: string,
+    zip: string,
+    phone: string
+}
+
+export type ShippingItem = {
+    variant_id: string,
+    external_variant_id: string,
+    warehouse_product_variant_id: string,
+    quantity: number,
+    value: string
+}
