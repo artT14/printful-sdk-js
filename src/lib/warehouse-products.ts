0a0901f21e5159355d6d0dfcbe57a94e3e1f24fc
ArtT14 <lamferme@gmail.com>
2023-07-12 19:51:11 -0700

    IMPLEMENT: WarehouseProductsAPI

--- src/lib/warehouse-products.ts
+++ src/lib/warehouse-products.ts
@@ -8,7 +8,49 @@ import type { Headers } from "../types/headers";
 export default class WarehouseProductsAPI extends GenericAPI{
     constructor(headers: Headers, origin: string){super(headers, origin)}
 
-    async getWarehouseProducts(){}
+    /**
+     * Returns a list of warehouse products from your store
+     * 
+     * @param {string} query - Filter by partial or full product name
+     * @param {number} offset - Number of items per page (max 100)
+     * @param {number} limit - Result set offset
+     * 
+     * @returns {promise} {products, paging, error}
+     */
+    async getWarehouseProducts(query: string = "", offset: number = 0, limit: number = 20){
+        const params = new URLSearchParams({});
+        query && params.append("query", query);
+        offset && params.append("offset", String(offset));
+        limit && params.append("limit", String(limit))
+        const url = this.origin + "/warehouse/products?" + params.toString();
+        const response = await fetch(url, {
+            headers: this.headers,
+        });
+        const data = await response.json();
+        const {result: products, paging, code, error} = await data;
+        if (code >= 400){
+            return {products: [], paging: {offset, limit}, error};
+        }
+        return {products, paging, error: {}};
+    }
 
-    async getWarehouseProduct(){}
+    /**
+     * Returns warehouse product data by ID
+     * 
+     * @param {int|string} id - Product ID
+     * 
+     * @returns {promise} {product, error}
+     */
+    async getWarehouseProduct(id: number | string){
+        const url = this.origin + "/warehouse/products/" + id;
+        const response = await fetch(url, {
+            headers: this.headers,
+        });
+        const data = await response.json();
+        const {result: product, code, error} = await data;
+        if (code >= 400){
+            return {product: {}, error};
+        }
+        return {product, error: {}};
+    }
 }
