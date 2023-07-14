357ac774828910c40f35faf11ed0ffc3c96d99c8
ArtT14 <lamferme@gmail.com>
2023-07-13 18:41:21 -0700

    Update descriptions on some unit test

--- test/lib/catalog.test.ts
+++ test/lib/catalog.test.ts
@@ -11,27 +11,27 @@ beforeAll(()=>{
 })
 
 describe("CatalogAPI", ()=>{
-	it("should get info about all products from catalog", async ()=>{
+	it("should return list of Products available in the Printful", async ()=>{
 		const {products, error} = await client.catalog.getAllProducts();
 		expect(error).toBeNull();
 		expect(products).toBeDefined();
 	});
 
-	it("should get info about a single product from catalog", async()=>{
+	it("should return information about a specific product and a list of variants for this product.g", async()=>{
         const {product, variants, error} = await client.catalog.getProduct(71);
 		expect(error).toBeNull();
 		expect(product).toBeDefined();
 		expect(variants).toBeDefined();
 	});
 
-	it("should get info about a specific variant and its product from catalog", async ()=>{
+	it("should return information about a specific Variant and its Product", async ()=>{
         const {product, variant, error} = await client.catalog.getVariant(4018);
 		expect(error).toBeNull();
 		expect(product).toBeDefined();
 		expect(variant).toBeDefined();
 	});
 
-	it("should get info about the size guide for a specific product from catalog", async ()=>{
+	it("should return information about the size guide for a specific product.", async ()=>{
         const {product_id, available_sizes, size_tables, error} = await client.catalog.getSize(71);
 		expect(error).toBeNull();
 		expect(product_id).toBe(71);
@@ -39,13 +39,13 @@ describe("CatalogAPI", ()=>{
 		expect(size_tables).toBeDefined();
 	})
 
-	it("should get info about all categories on catalog", async ()=>{
+	it("should return list of Catalog Categories available in the Printful", async ()=>{
 		const {categories, error} = await client.catalog.getAllCategories();
 		expect(error).toBeNull();
 		expect(categories).toBeDefined();
 	})
 
-	it("should get information about a specific category", async ()=>{
+	it("should return information about a specific category", async ()=>{
 		const {category, error} = await client.catalog.getCategory(24);
 		expect(error).toBeNull();
 		expect(category).toBeDefined();
---
+++ test/lib/products.test.ts
@@ -0,0 +1,12 @@
+import {createPrintfulStoreClient, PrintfulStoreClient} from "../../src/client";
+require('dotenv').config()
+
+// NOTE: These Tests are optimistic, see TODO 
+// TODO: Needs more Tests, especially for negative outcomes, and bad inputs
+
+let client: PrintfulStoreClient;
+
+beforeAll(()=>{
+    client = createPrintfulStoreClient(process.env.TEST_AUTH);
+})
+
